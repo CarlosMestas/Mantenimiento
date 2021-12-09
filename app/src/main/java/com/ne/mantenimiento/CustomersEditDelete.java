d851d1e034621620f0c2725b3d7c95ec37fe74c9
CarlosMestas <42045414+CarlosMestas@users.noreply.github.com>
2021-12-09 12:23:59 -0500

    Final commit :v

--- app/src/main/java/com/ne/mantenimiento/CustomersEditDelete.java
+++ app/src/main/java/com/ne/mantenimiento/CustomersEditDelete.java
@@ -12,123 +12,121 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class CategoriasEditDelete extends AppCompatActivity {
+public class CustomersEditDelete extends AppCompatActivity {
 
-    private EditText editTextId, editTextName, editTextDescription, editTextStatus;
+    private EditText editTextId, editTextName, editTextDni, editTextStatus;
     private Button buttonEdit, buttonDelete, buttonCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categorias_edit_delete);
+        setContentView(R.layout.activity_customers_edit_delete);
 
-        editTextId = findViewById(R.id.categoriasEDId);
-        editTextName = findViewById(R.id.categoriasEDname);
-        editTextDescription = findViewById(R.id.categoriasEDDescription);
-        editTextStatus = findViewById(R.id.categoriasEDStatus);
+        editTextId = findViewById(R.id.customersEDId);
+        editTextName = findViewById(R.id.customersEDname);
+        editTextDni = findViewById(R.id.customersEDDni);
+        editTextStatus = findViewById(R.id.customersEDStatus);
 
-        buttonEdit = findViewById(R.id.categoriasEDButtonEdit);
-        buttonDelete = findViewById(R.id.categoriasEDButtonDelete);
-        buttonCancel = findViewById(R.id.categoriasEDButtonCancel);
+        buttonEdit = findViewById(R.id.customersEDButtonEdit);
+        buttonDelete = findViewById(R.id.customersEDButtonDelete);
+        buttonCancel = findViewById(R.id.customersEDButtonCancel);
 
         Intent intent = getIntent();
 
         String id  = intent.getStringExtra("id").toString();
         String nam = intent.getStringExtra("nam").toString();
-        String des = intent.getStringExtra("des").toString();
+        String dni = intent.getStringExtra("dni").toString();
         String est = intent.getStringExtra("est").toString();
 
         editTextId.setText(id);
         editTextName.setText(nam);
-        editTextDescription.setText(des);
+        editTextDni.setText(dni);
         editTextStatus.setText(est);
-
 
         buttonEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editCategory();
+                editCustomer();
             }
         });
 
         buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                deleteCategory();
+                deleteCustomer();
             }
         });
 
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CategoriasEditDelete.this, Menu.class);
+                Intent intent = new Intent(CustomersEditDelete.this, Menu.class);
                 startActivity(intent);
             }
         });
 
     }
 
-    public void editCategory(){
+    public void editCustomer(){
         try {
-            String idE, nameE, descriptionE, statusE;
+            String idE, nameE, dniE, statusE;
 
             idE = editTextId.getText().toString();
             nameE = editTextName.getText().toString();
-            descriptionE = editTextDescription.getText().toString();
+            dniE = editTextDni.getText().toString();
             statusE = editTextStatus.getText().toString();
 
             SQLiteDatabase db = openOrCreateDatabase("ne", Context.MODE_PRIVATE, null);
 
-            String sql = "update category set catNam = ?, catDes = ?, catEst = ? where id = ?";
+            String sql = "update customer set cusNam = ?, cusDni = ?, cusEst = ? where id = ?";
 
             SQLiteStatement statement = db.compileStatement(sql);
             statement.bindString(1, nameE);
-            statement.bindString(2, descriptionE);
+            statement.bindString(2, dniE);
             statement.bindString(3, statusE);
             statement.bindString(4, idE);
 
             statement.execute();
-            Toast.makeText(this, "Categoría modificada", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Cliente modificado", Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(getApplicationContext(), Menu.class);
             startActivity(intent);
 
         }
         catch (Exception e){
-            Toast.makeText(this, "Categoria no modificada", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Cliente no modificado", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void deleteCategory(){
+    public void deleteCustomer(){
         try {
-            String idE, nameE, descriptionE, statusE;
+            String idE, nameE, dniE, statusE;
 
             idE = editTextId.getText().toString();
             nameE = editTextName.getText().toString();
-            descriptionE = editTextDescription.getText().toString();
+            dniE = editTextDni.getText().toString();
             statusE = editTextStatus.getText().toString();
 
             SQLiteDatabase db = openOrCreateDatabase("ne", Context.MODE_PRIVATE, null);
 
-            String sql = "update category set catNam = ?, catDes = ?, catEst = ? where id = ?";
+            String sql = "update customer set cusNam = ?, cusDni = ?, cusEst = ? where id = ?";
 
             SQLiteStatement statement = db.compileStatement(sql);
             statement.bindString(1, nameE);
-            statement.bindString(2, descriptionE);
+            statement.bindString(2, dniE);
             statement.bindString(3, "*");
             statement.bindString(4, idE);
 
             statement.execute();
-            Toast.makeText(this, "Categoría eliminada", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Cliente modificado", Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(getApplicationContext(), Menu.class);
             startActivity(intent);
 
         }
         catch (Exception e){
-            Toast.makeText(this, "Categoria no eliminada", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Cliente no modificado", Toast.LENGTH_SHORT).show();
         }
     }
-
 
 }
--- app/src/main/java/com/ne/mantenimiento/Menu.java
+++ app/src/main/java/com/ne/mantenimiento/Menu.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 public class Menu extends AppCompatActivity {
 
     private Button buttonCategoriasAdd, buttonCategoriasList;
+    private Button buttonCustomersAdd, buttonCustomersList;
+    private Button buttonProductsAdd, buttonProductsList;
+    private Button buttonSalesAdd, buttonSalesList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,68 @@ public class Menu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        buttonCustomersAdd = findViewById(R.id.menuButtonCustomersAdd);
+        buttonCustomersList = findViewById(R.id.menuButtonCustomersList);
+
+        buttonCustomersAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, Customers.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonCustomersList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, CustomersList.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonProductsAdd = findViewById(R.id.menuButtonProductsAdd);
+        buttonProductsList = findViewById(R.id.menuButtonProductsList);
+
+        buttonProductsAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, Products.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonProductsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, ProductsList.class);
+                startActivity(intent);
+            }
+        });
+
+
+        buttonSalesAdd = findViewById(R.id.menuButtonSalesAdd);
+        buttonSalesList = findViewById(R.id.menuButtonSalesList);
+
+        buttonSalesAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, Sales.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSalesList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, SalesList.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
 
 
